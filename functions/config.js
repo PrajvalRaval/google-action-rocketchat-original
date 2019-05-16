492743155b90673faa2d99b9d365004fe2bf11ae
PrajvalRaval <41849970+PrajvalRaval@users.noreply.github.com>
2019-05-16 20:45:25 +0530

    Minor Whitespace Fix

--- functions/config.js
+++ functions/config.js
@@ -4,5 +4,5 @@ const functions = require('firebase-functions');
 module.exports = {
     SERVER_URL : functions.config().envariables.server_url,
     CLIENT_ID : functions.config().envariables.clientid,
-	OAUTH_SERVICE_NAME : functions.config().envariables.oauth_service_name
+    OAUTH_SERVICE_NAME : functions.config().envariables.oauth_service_name
 };
